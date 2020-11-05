84b4a4fe5fecd6b8d5a623ae1c0af61d3efc3b0d
Vladimir Bykov <vladimir-bykov-1999@mail.ru>
2020-11-05 15:02:02 +0300

    1. Создание Solution для проекта GameStore 2. Создание БД 3. Создание класса модели данных 4. Создание контекста Entity Framework 5. Создание хранилища товаров 6. Создание списка товаров

---
+++ GameStore/GameStore/Models/Repository/Repository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace GameStore.Models.Repository
+{
+    public class Repository
+    {
+        private EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Game> Games
+        {
+            get { return context.Games; }
+        }
+    }
+}
---
+++ GameStore/GameStore/Pages/Listing.aspx.cs
@@ -0,0 +1,22 @@
+﻿using GameStore.Models;
+using GameStore.Models.Repository;
+using System;
+using System.Collections.Generic;
+
+namespace GameStore.Pages
+{
+    public partial class Listing : System.Web.UI.Page
+    {
+        private Repository repository = new Repository();
+
+        protected IEnumerable<Game> GetGames()
+        {
+            return repository.Games;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
