222d1d99d30ab8ce85f6329df4ebfcb01486f541
Vladimir Bykov <vladimir-bykov-1999@mail.ru>
2020-11-16 20:01:03 +0300

    1. Расширение хранилища 2. Добавление веб-формы 3. Создание методов CRUD 4. Тестирование средств управления каталогом

--- GameStore/GameStore/Models/Repository/Repository.cs
+++ GameStore/GameStore/Models/Repository/Repository.cs
@@ -23,6 +23,43 @@ namespace GameStore.Models.Repository
             }
         }
 
+        //добавление, редактирование и удаление записей о товарах в БД
+        public void SaveGame(Game game)
+        {
+            if (game.GameId == 0)
+            {
+                game = this.context.Games.Add(game);
+            }
+            else
+            {
+                Game dbGame = this.context.Games.Find(game.GameId);
+                if (dbGame != null)
+                {
+                    dbGame.Name = game.Name;
+                    dbGame.Description = game.Description;
+                    dbGame.Price = game.Price;
+                    dbGame.Category = game.Category;
+                }
+            }
+            this.context.SaveChanges();
+        }
+
+        public void DeleteGame(Game game)
+        {
+            IEnumerable<Order> orders = this.context.Orders
+                .Include(o => o.OrderLines.Select(ol => ol.Game))
+                .Where(o => o.OrderLines
+                    .Count(ol => ol.Game.GameId == game.GameId) > 0)
+                .ToArray();
+
+            foreach(Order order in orders)
+            {
+                this.context.Orders.Remove(order);
+            }
+            this.context.Games.Remove(game);
+            this.context.SaveChanges();
+        }
+
         // Сохранить данные заказа в базе данных
         public void SaveOrder(Order order)
         {
